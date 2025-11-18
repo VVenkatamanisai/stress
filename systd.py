de28a567152962d005e730e18cf3e341bdfce114
Arpit Tripathi <cs20resch11006@iith.ac.in>
2025-11-18 15:12:33 +0530

    Update systd.py

--- systd.py
+++ systd.py
@@ -1,14 +1,3 @@
-#!/usr/bin/env python3
-"""
-gen_syslogs.py
-Generate messages that end up in the system journal (viewable with journalctl).
-
-Usage examples:
-  python3 gen_syslogs.py --rate 1           # 1 message per second
-  python3 gen_syslogs.py --rate 5 --count 10 --tag myapp
-  python3 gen_syslogs.py --rate 0.5 --level WARNING
-"""
-
 import time
 import argparse
 import logging
